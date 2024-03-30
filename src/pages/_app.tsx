88fbc46df398a6478add1eb8b049a5d4773bd4df
Vigneshwaran <86647561+Vignesh2828@users.noreply.github.com>
2024-03-30 17:53:11 +0530

    speed insights from vercel imported

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "@/components/store/store";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
