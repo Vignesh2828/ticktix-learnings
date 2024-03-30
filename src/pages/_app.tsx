a2a1deef2e77ef62509fefc3eaa24e5ccbb8902d
Vigneshwaran <86647561+Vignesh2828@users.noreply.github.com>
2024-03-30 17:54:05 +0530

    app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Provider store={store}>
         <Component {...pageProps} />
+        <SpeedInsights />
       </Provider>
     </>
   );
